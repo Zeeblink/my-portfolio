c8abf1d2cdd033149b689e87afcd353ef59f8577
Zeeblink <eabiodun45@gmail.com>
2025-04-17 09:59:55 +0100

    Added canonical link in Head component for SEO optimization.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,16 +7,19 @@ const ExperienceSection = dynamic(() => import('@/components/sections/Experience
 const ProjectsSection = dynamic(() => import('@/components/sections/Projects'))
 const ContactSection = dynamic(() => import('@/components/sections/Contact'))
 const ScrollProgressBar = dynamic(() => import('@/components/animated/ScrollProgessBar'))
-import { client }from '@/sanity-lib/client'
+import { client } from '@/sanity-lib/client'
 import { latestPostsQuery } from '@/sanity-lib/queries'
 import { BlogSection } from '@/components/sections/BlogSection'
-
+import Head from 'next/head'
 
 export default async function Portfolio() {
   const latestPosts = await client.fetch(latestPostsQuery)
-  
+
   return (
     <>
+      <Head>
+        <link rel="canonical" href="https://emmanueloye.xyz/" />
+      </Head>
       <ScrollProgressBar />
       <HomeSection />
       <AboutSection />
