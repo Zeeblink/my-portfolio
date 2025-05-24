ab9e41323950a9230d35fbfecfaa3ff5690bf602
Zeeblink <eabiodun45@gmail.com>
2025-05-24 10:49:44 +0100

    Token config

--- frontend/sanity-lib/config.ts
+++ frontend/sanity-lib/config.ts
@@ -4,6 +4,6 @@ export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
     apiVersion: '2025-03-18',
     useCdn: true,
-    token: process.env.SANITY_API_TOKEN, // Optional, for write operations
+    token: process.env.SANITY_REVALIDATE_SECRET, // Optional, for write operations
   }
   
